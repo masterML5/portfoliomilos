18a91257651990f2ddb99b39f13801d89a2ba617
masterML5 <milosjelic98@gmail.com>
2022-04-14 01:26:38 +0200

    exp

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id="about">about</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about img" />
+          </div>
+        </div>
+          <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward className='about__icon' />
+                <h5>Experience</h5>
+                <small>3+ Years Working</small>
+              </article>
+              <article className='about__card'>
+                <FaAward className='about__icon' />
+                <h5>Experience</h5>
+                <small>3+ Years Working</small>
+              </article>
+              <article className='about__card'>
+                <FaAward className='about__icon' />
+                <h5>Experience</h5>
+                <small>3+ Years Working</small>
+              </article>
+            </div>
+          </div>
+      </div>
+    </section>
   )
 }
 
