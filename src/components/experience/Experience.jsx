45f69757cadb7044795e942f46979e0b68da3d50
masterML5 <milosjelic98@gmail.com>
2022-04-14 23:36:21 +0200

    novi update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -12,35 +12,35 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>HTML</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>CSS</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
              <div>
              <h4>JavaScript</h4> 
               <small className='text-light'>Experienced</small>
              </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
              <div>
              <h4>Bootstrap</h4> 
               <small className='text-light'>Experienced</small>
              </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>React.js</h4> 
               <small className='text-light'>Experienced</small>
@@ -53,35 +53,35 @@ const Experience = () => {
         <h3>Backend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>PHP</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>Java</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>MySQL</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon' />
               <div>
               <h4>Node.js</h4> 
               <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsCheckCircleFill />
+              <BsCheckCircleFill className='experience__details-icon'/>
               <div>
               <h4>Python</h4> 
               <small className='text-light'>Experienced</small>
