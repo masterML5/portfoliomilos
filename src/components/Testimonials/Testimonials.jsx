eb9d43bdfb7b16bd1409282c5f16c9c004e59150
masterML5 <milosjelic98@gmail.com>
2022-04-15 21:01:50 +0200

    contact gotov

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.png'
+import AVTR2 from '../../assets/avatar2.png'
+import AVTR3 from '../../assets/avatar3.png'
+import AVTR4 from '../../assets/avatar4.png'
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Show',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt natus amet nemo cumque reprehenderit corrupti ipsam maiores optio ullam eveniet magnam facere iusto molestias, accusamus molestiae numquam. Beatae, sit."
+  },
+  {
+    avatar: AVTR2,
+    name: 'Pera Show',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt natus amet nemo cumque reprehenderit corrupti ipsam maiores optio ullam eveniet magnam facere iusto molestias, accusamus molestiae numquam. Beatae, sit."
+  },
+  {
+    avatar: AVTR3,
+    name: 'Mika Show',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt natus amet nemo cumque reprehenderit corrupti ipsam maiores optio ullam eveniet magnam facere iusto molestias, accusamus molestiae numquam. Beatae, sit."
+  },
+  {
+    avatar: AVTR4,
+    name: 'Zika Show',
+    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sunt natus amet nemo cumque reprehenderit corrupti ipsam maiores optio ullam eveniet magnam facere iusto molestias, accusamus molestiae numquam. Beatae, sit."
+  }
+]
 const Testimonials = () => {
   return (
-    <section id='testimonials'>test</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}  
+      pagination={{ clickable: true }}
+      >
+       {
+         data.map(({avatar, name, review}, index) => {
+           return(
+            <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={avatar} alt="Avatar one" />
+            </div>
+            <h5 className='client__name'>{name}</h5>
+              <small className="client__review">
+               {review}
+              </small>
+          </SwiperSlide>
+           )
+         })
+       }
+      </Swiper>
+    </section>
   )
 }
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import {FaFacebookMessenger} from 'react-icons/fa'
+import {FaWhatsapp} from 'react-icons/fa'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+
+
+
+let newElement = document.createElement('label');
+newElement.id = 'poruka'
+newElement.innerText='Uspesno ste poslali poruku';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_yyr0mjm', 'template_erw6yre', form.current, 'KBdeFNgaXFZEDMx6U')
+      
+    e.target.reset()
+    
+    document.querySelector('form').appendChild(newElement);
+    
+    
+  };
   return (
-    <section id='contact'>cont</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail  className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>milosjelic98@gmail.com</h5>
+            <a href="mailto:milosjelic98@gmail.com">Send a message</a>     
+          </article>
+          <article className="contact__option">
+            <FaFacebookMessenger className='contact__option-icon' />
+            <h4>Messanger</h4>
+            <h5>Milos Jelic</h5>
+            <a href="https://m.me/masterML5" target="_blank" rel='noreferrer'>Send a message</a>     
+          </article>
+          <article className="contact__option">
+            <FaWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>Milos Jelic</h5>
+            <a href="https://api.whatsapp.com/send?phone=+381614819611" target="_blank" rel='noreferrer'>Send a message</a>     
+          </article>
+          </div>
+          {/*end*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='name' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <label id='poruka'></label>
+        </form>
+      
+      </div>
+      
+    </section>
   )
 }
 
