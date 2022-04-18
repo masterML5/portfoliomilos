74ef959c91195ab95280d3c590fe304041adc16f
masterML5 <milosjelic98@gmail.com>
2022-04-18 10:04:12 +0200

    gotovo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {AiFillTwitterCircle} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Milos Jelic </a>
+      <ul className='permalinks'>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <AiFillTwitterCircle />
+        </a>
+        </div>
+
+        <div className="footer__copyright">
+         <small> Milos Jelic &copy;  All rights reserved</small>
+        </div>
+    </footer>
   )
 }
 
